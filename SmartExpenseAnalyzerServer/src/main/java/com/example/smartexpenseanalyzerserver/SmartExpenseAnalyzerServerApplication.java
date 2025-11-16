c4969a81e694b319c3b28f9ac3adcaffbdd77506
Cosmin Popovici <cosmin_popovici2012@yahoo.com>
2025-11-16 14:58:27 +0200

    Added backend project with postgres sql

---
+++ SmartExpenseAnalyzerServer/src/main/java/com/example/smartexpenseanalyzerserver/SmartExpenseAnalyzerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartexpenseanalyzerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseAnalyzerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartExpenseAnalyzerServerApplication.class, args);
+    }
+
+}
---
+++ SmartExpenseAnalyzerServer/src/test/java/com/example/smartexpenseanalyzerserver/SmartExpenseAnalyzerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartexpenseanalyzerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpenseAnalyzerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
